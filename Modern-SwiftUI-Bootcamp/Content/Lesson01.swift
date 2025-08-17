cc5c1c105a85a8b4e31a51cccbd4db024cb1eef5
Ertan Şinik <ertansinik@gmail.com>
2025-08-17 21:31:55 +0300

    Bootcamp Ders 1

---
+++ Modern-SwiftUI-Bootcamp/Content/Lesson01.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Modern-SwiftUI-Bootcamp
+//
+//  Created by Ertan Şinik on 17.08.2025.
+//
+
+import Foundation
+
+// MARK: - ÖDEV 1.1 Başlangıcı
+// ÖDEV 1.1
+print("********* ÖDEV 1.1 *******")
+
+print("Hello World! (Yazmayanlar yazılımcı olamıyor diye duymuştum)")
+
+// Kişisel bilgi kartı
+var adSoyad: String = "Ertan Şinik"
+var yas: Int = 39
+var yeniBaslayanMi: Bool = false
+var kacYillik: Double = 14.3
+
+// Optional değişken örneği
+var username: String? = nil
+
+// Optional güvenli açılım
+if let safeUsername = username {
+    print("Kullanıcı adı: \(safeUsername)")
+} else {
+    print("Kullanıcı ad girilmemiş.")
+}
+
+// Yazdırılıyor
+print("Ad: \(adSoyad)")
+print("Yaş: \(yas)")
+print("Yeni Başlayan Mı?: \(yeniBaslayanMi)")
+print("Yıl: \(kacYillik)")
+
+print("Kullanıcı: \(username ?? "Bilinmiyor")")
+// MARK: - ÖDEV 1.1 bitişi
+
